33eb0a91a000db8d6ed7826fef8d249c87783551
aniket <aniketpakhare32@gmail.com>
2026-02-25 21:08:15 +0530

    first commit

---
+++ InterestService.Repository/Migrations/20260221142005_addcolumn.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterestService.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ClosingBalance",
+                table: "EmiSchedules",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "OpeningBalance",
+                table: "EmiSchedules",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClosingBalance",
+                table: "EmiSchedules");
+
+            migrationBuilder.DropColumn(
+                name: "OpeningBalance",
+                table: "EmiSchedules");
+        }
+    }
+}
---
+++ InterestService/Controllers/EmiController.cs
@@ -0,0 +1,39 @@
+using InterestService.Application.DTO;
+using InterestService.Application.Interfaces;
+using InterestService.Repository.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace InterestService.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class EmiController : ControllerBase
+  {
+
+    IEmi emiservice;
+
+    public EmiController(IEmi e) {
+      emiservice = e;
+    }
+
+    [HttpPost("generate-emi")]
+    public async Task<IActionResult> GenerateEmi([FromBody] EmiRequestDTO request)
+    {
+      var data =await emiservice.GenerateSchedule(request);
+            var response = new ApiResponse<object>
+            {
+                Success = true,
+                StatusCode = 200,
+                Message = "Loan Schedule generated successfully",
+                Data = data,
+                Errors = null,
+                Meta = new { Timestamp = DateTime.UtcNow }
+            };
+            return Ok(response);
+    }
+
+   
+  }
+}
